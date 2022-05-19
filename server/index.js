e9603725ebf65e46058436657407aede94c56709
roelineburger <93773777+roelineburger@users.noreply.github.com>
2022-05-19 10:49:43 +0200

    Connection to MongoDB Atlas established

--- server/index.js
+++ server/index.js
@@ -2,3 +2,30 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+// this says every route inside of post routes will start with posts
+app.use("/posts", postRoutes);
+// limit is because we are going to be sending images
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// use mongoDB with Cloud Atlas create cluster
+const CONNECTION_URL =
+  "mongodb+srv://roelineb:6S9w5Q99UdGa7JR5@cluster0.o0lc1.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    })
+  )
+  .catch((error) => console.log(error.message));
